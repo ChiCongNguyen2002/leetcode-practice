971c8eb1f4cf570b13303d4fbe33657aea307caf
ChiCongNguyen2002 <171400073+ChiCongNguyen2002@users.noreply.github.com>
2026-05-11 21:02:20 +0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.go
@@ -0,0 +1,88 @@
+// func topKFrequent(nums []int, k int) []int {
+// 	// constraints: len(nums) >= 1
+// 	// k >= 1
+// 	// arr: not sorted
+	
+// 	// find element frequent >= k
+// 	// input : 1,2,2,3,3,3 k = 2
+// 	// output: [1,2]
+
+// 	// the mindset presentation: 
+// 	// [1: 1]
+// 	// [2: 2]
+// 	// [3: 3] 
+// 	// k := 2
+// 	// output : find elemenet >= 2 freq 
+// 	// [2 , 3] codition freq >= k
+
+// 	// step 1: map save freq element
+// 	// step 2: save array count freq 
+// 	// step 3: compare freq map : with k 
+// 	// step 4: output freq >= k 
+
+// 	type Freq struct {
+// 		val int
+// 		freq int
+// 	}
+
+// 	// step 1
+// 	mapFreq := make(map[int]int)
+// 	for _,num := range nums {
+// 		mapFreq[num]++ 
+// 	}
+
+// 	// step 2
+
+// 	var result []Freq
+// 	for num, freq := range mapFreq {
+// 		result = append(result, Freq{
+// 			val : num,
+// 			freq: freq,
+// 		})
+// 	} 
+// 	// result :
+// 	// [1,1], [2,2] [3,3]
+	
+// 	// sort freq 
+// 	sort.Slice(result, func (i, j int) bool{
+// 		return result[i].freq > result[j].freq
+// 	})
+// 	// [3,3] , [2,2], [1,1]
+
+// 	for i:=0;i<len(result);i++ {
+// 		fmt.Println("result",result[i])
+// 	}
+
+// 	var output[]int
+// 	for i:=0;i<k;i++ {
+// 		output = append(output, result[i].val)
+// 	}
+
+// 	return output
+// }
+
+
+func topKFrequent(nums []int, k int) []int {
+	// step 1
+	mapFreq := make(map[int]int)
+	for _,num := range nums {
+		mapFreq[num]++ 
+	}
+
+	// step 2
+
+	buckets := make([][]int,len(nums) + 1)
+	for num, freq := range mapFreq {
+		buckets[freq] = append(buckets[freq],num)
+	}
+
+	var output[]int
+	for i:=len(buckets) - 1 ; i >= 0 && len(output) < k;i --{
+		if buckets[i] != nil {
+			output = append(output, buckets[i]...)
+		}
+	}
+
+	return output[:k]
+}
+
